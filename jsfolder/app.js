39e2f71f603ff2ca753eb8094a737cbde87ad68b
afraemahsan <afraemahsan24@gmail.com>
2022-08-23 10:45:13 +0600

    fourth update

---
+++ jsfolder/app.js
@@ -0,0 +1,17 @@
+// const buttonName = document.getElementById("addto-list");
+
+// this.addEventListener("click", function () {
+//   const playerName = document.getElementById("player-name");
+//   const playerInfo = playerName.innerText;
+//   console.log(playerInfo);
+//   const selecTion = document.getElementById("selection");
+//   const ol = document.createElement("ol");
+//   const olshow = selecTion.appendChild(ol);
+//   ol.style.color = "white";
+//   const li = document.createElement("li");
+//   const liShow = ol.appendChild(li);
+//   li.style.color = "white";
+//   liShow.innerText = playerInfo;
+//   buttonName.setAttribute("disabled", "");
+//   buttonName.style.backgroundColor = "gray";
+// });
